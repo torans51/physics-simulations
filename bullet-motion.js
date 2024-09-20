e8f4124605c253e3ef94ce96347e68156e488559
torans <>
2024-09-20 21:14:52 +0200

    Add basic gui controls

--- bullet-motion.js
+++ bullet-motion.js
@@ -15,6 +15,10 @@ const initBulletMotion = (containerId) => {
   })
   stage.add(layer)
 
+  const gui = new dat.GUI()
+  const guiData = {
+    running: true,
+  }
   const state = {
     stageWidth: STAGE_WIDTH,
     stageHeight: STAGE_HEIGHT,
@@ -25,17 +29,41 @@ const initBulletMotion = (containerId) => {
     prevTimestamp: null, // (milliseconds)
     deltaT: null, // timestamp - prevTimestamp (milliseconds)
     pos: { x: 0, y: 0 }, // position of point
-    speed: { x: 0.1, y: 0.1 }, // speed of point
+    speed: { x: 100, y: 100 }, // speed of point
+    acc: { x: 0, y: -40 }, // acceleration of point
+
+    // UI options
+    guiData,
+    timeFactor: 0.0001, // increase or decrease simulation speed
+    displayPos: false,
+    displayPosX: false,
+    displayPosY: false,
+    displaySpeed: false,
+    displaySpeedX: false,
+    displaySpeedY: false,
+    displayAcc: false,
+    displayAccX: false,
+    displayAccY: false,
   }
+
+  gui.add(state.guiData, "running").name("Run").onChange(value => {
+    state.guiData.running = !!value
+  })
+
   state.stage = stage
 
   function updateState(state) {
-    const { deltaT, pos, speed } = state
+    const { deltaT, pos, speed, acc } = state
     nextPos = {
-      x: speed.x * deltaT + pos.x,
-      y: speed.y * deltaT + pos.y,
+      x: pos.x + speed.x * deltaT + 0.5 * acc.x * deltaT * deltaT,
+      y: pos.y + speed.y * deltaT + 0.5 * acc.y * deltaT * deltaT,
+    }
+    nextSpeed = {
+      x: speed.x + acc.x * deltaT,
+      y: speed.y + acc.y * deltaT,
     }
     state.pos = nextPos
+    state.speed = nextSpeed
   }
 
   function drawPoint(state) {
@@ -46,8 +74,68 @@ const initBulletMotion = (containerId) => {
       fill: 'blue',
     })
 
-    console.log(circle)
     state.layer.add(circle)
+  }
+
+  function drawPos(state) {
+    const { pos } = state
+
+    if (state.displayPos) {
+      const arrow = new Konva.Arrow({
+        points: [0, 0, pos.x, pos.y],
+        stroke: "purple",
+        strokeWidth: 3,
+      })
+      state.layer.add(arrow)
+    }
+    if (state.displayPos && state.displayPosX) {
+      const arrow = new Konva.Arrow({
+        points: [0, 0, pos.x, 0],
+        stroke: "purple",
+        strokeWidth: 3,
+        dash: [5, 5],
+      })
+      state.layer.add(arrow)
+    }
+    if (state.displayPos && state.displayPosY) {
+      const arrow = new Konva.Arrow({
+        points: [0, 0, 0, pos.y],
+        stroke: "purple",
+        strokeWidth: 3,
+        dash: [5, 5],
+      })
+      state.layer.add(arrow)
+    }
+  }
+  function drawSpeed(state) {
+    const { pos, speed } = state
+
+    if (state.displaySpeed) {
+      const arrow = new Konva.Arrow({
+        points: [pos.x, pos.y, pos.x + speed.x, pos.y + speed.y],
+        stroke: "red",
+        strokeWidth: 3,
+      })
+      state.layer.add(arrow)
+    }
+    if (state.displaySpeed && state.displaySpeedX && speed.x !== 0) {
+      const arrow = new Konva.Arrow({
+        points: [pos.x, pos.y, pos.x, pos.y + speed.y],
+        stroke: "red",
+        strokeWidth: 3,
+        dash: [5, 5],
+      })
+      state.layer.add(arrow)
+    }
+    if (state.displaySpeed && state.displaySpeedY && speed.x !== 0) {
+      const arrow = new Konva.Arrow({
+        points: [pos.x, pos.y, pos.x + speed.x, pos.y],
+        stroke: "red",
+        strokeWidth: 3,
+        dash: [5, 5],
+      })
+      state.layer.add(arrow)
+    }
   }
 
   function drawAxis(state) {
@@ -73,9 +161,21 @@ const initBulletMotion = (containerId) => {
       state.timestamp = timestamp
       state.deltaT = timestamp - state.prevTimestamp
 
-      updateState(state)
-      drawPoint(state)
+      // UI options
+      state.deltaT *= state.timeFactor
+      state.displayPos = true
+      // state.displayPosX = true
+      // state.displayPosY = true
+      state.displaySpeed = true
+      state.displaySpeedX = true
+      state.displaySpeedY = true
+
+      if (state.guiData.running) updateState(state)
+
       drawAxis(state)
+      drawPoint(state)
+      drawPos(state)
+      drawSpeed(state)
 
       state.layer.draw()
       state.prevTimestamp = timestamp
